dd0af3e238d104d2a5f9458179b67a0cf1e93a04
Fabio-Cristovao <fabio.ascristovao@gmail.com>
2022-06-29 17:14:02 +0100

    Updates

---
+++ precache-manifest.2df45e0fad844018f1b87bf65a19e518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f004ae0d24f1141a9e53bee6b9392623",
+    "url": "/FICO_Merch/index.html"
+  },
+  {
+    "revision": "ddd936898d2da4e26ad3",
+    "url": "/FICO_Merch/static/js/2.e3ae1d35.chunk.js"
+  },
+  {
+    "revision": "474df8823e76a299e382915c20ee49c3",
+    "url": "/FICO_Merch/static/js/2.e3ae1d35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ed179fa324fdec1fa2",
+    "url": "/FICO_Merch/static/js/main.5946fe35.chunk.js"
+  },
+  {
+    "revision": "4c880eff71ffc16cc6d6",
+    "url": "/FICO_Merch/static/js/runtime-main.a8906ddf.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/FICO_Merch/static/media/commerce.422fec10.png"
+  }
+]);
